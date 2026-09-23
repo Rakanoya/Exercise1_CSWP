a79dad9e1163b56208ece3300d5655a54d2f2c29
Kaitlyn Dennis <156827880+Rakanoya@users.noreply.github.com>
2026-09-23 16:04:11 +0000

    exercise 17

--- scripts/exercise17.js
+++ scripts/exercise17.js
@@ -1,7 +1,14 @@
 function startClock() {
   setInterval(() => {
     const now = new Date();
-    console.log(now.toLocaleTimeString());
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const time = now.getHours() + ":" + minutes + ":" + seconds;
+
+    const timeHeader = document.getElementById("timeheader");
+    if (timeHeader) {
+      timeHeader.textContent = time;
+    }
   }, 1000);
 }
 
